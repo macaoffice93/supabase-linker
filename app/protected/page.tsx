35d6ed620412fa860aea00fb7c3698f69f976ea4
Maca office <macaoffice93@gmail.com>
2025-01-17 14:09:36 +0400

    remove reference to removed component

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,7 +2,6 @@ import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
-import DeploymentForm from "@/components/DeploymentForm";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
